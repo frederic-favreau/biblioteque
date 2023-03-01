ba39c9ed451f45008ff0f514ae0daae2baa6d5e9
frederic-favreau <frederic.favreau22@gmail.com>
2023-03-01 16:58:54 +0100

    commit initial - debut du projet

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biblook</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <header id="main-header">
+    <nav id="main-nav-bar" class="row-limit-size">
+        <div id="container-group-logo">
+            <img src="./img/logo-biblook (3).svg" alt="Biblook">
+            <span id="nav-logo"></span>
+        </div>
+        <div id="container-group-search-nav-top">
+            <input type="text" id="input-search-nav-top">
+            <a href="#" id="btn-search-nav-top">Rechercher</a>
+        </div>
+        <div id="container-group-btn-connexion">
+            <a href="./front/connect.php" id="btn-log-in">Log in</a>
+            <a href="./front/connect.php" id="btn-sign-up" >Sign up</a>
+        </div>
+    </nav>
+    </header>
+</body>
+</html>
