6157f72028db890ebabf84d444ae585deb2e5d84
frederic-favreau <frederic.favreau22@gmail.com>
2023-03-01 21:49:23 +0100

    Add SCSS page, connexion page, header-main page

--- front/connect.php
+++ front/connect.php
@@ -1,77 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connexion</title>
-    <link rel="stylesheet" href="../css/style.css">
+    <title>Authentification</title>
+    <link rel="stylesheet" href="../css/style-connect.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Emilys+Candy&family=Hind+Madurai&family=Nova+Slim&display=swap" rel="stylesheet">
 </head>
+
 <body>
     <main>
-      <section id="section-authentication">
-        <div class="container" id="container">
-          <div class="form-container sign-up-container">
-            <form action="#">
-              <h1>Create Account</h1>
-              <div class="social-container">
-                <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-                <a href="#" class="social"
-                  ><i class="fab fa-google-plus-g"></i
-                ></a>
-                <a href="#" class="social"
-                  ><i class="fab fa-linkedin-in"></i
-                ></a>
-              </div>
-              <span>or use your email for registration</span>
-              <input type="text" placeholder="Name" />
-              <input type="email" placeholder="Email" />
-              <input type="password" placeholder="Password" />
-              <button>Sign Up</button>
-            </form>
-          </div>
-          <div class="form-container sign-in-container">
-            <form action="#">
-              <h1>Sign in</h1>
-              <div class="social-container">
-                <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-                <a href="#" class="social"
-                  ><i class="fab fa-google-plus-g"></i
-                ></a>
-                <a href="#" class="social"
-                  ><i class="fab fa-linkedin-in"></i
-                ></a>
-              </div>
-              <span>or use your account</span>
-              <input type="email" placeholder="Email" />
-              <input type="password" placeholder="Password" />
-              <a href="#">Forgot your password?</a>
-              <button>Sign In</button>
-            </form>
-          </div>
-          <div class="overlay-container">
-            <div class="overlay">
-              <div class="overlay-panel overlay-left">
-                <h1>Welcome Back!</h1>
-                <p>
-                  To keep connected with us please login with your personal info
-                </p>
-                <button class="ghost" id="signIn">Sign In</button>
-              </div>
-              <div class="overlay-panel overlay-right">
-                <h1>Hello, Friend!</h1>
-                <p>Enter your personal details and start journey with us</p>
-                <button class="ghost" id="signUp">Sign Up</button>
-              </div>
+        <section id="section-authentication" class="row-limit-size">
+            <div class="container" id="container">
+                <div class="form-container sign-up-container">
+                    <form action="#">
+                        <h1>S'enregistrer</h1>
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <button>Sign Up</button>
+                    </form>
+                </div>
+                <div class="form-container sign-in-container">
+                    <form action="#">
+                        <h1>Connexion</h1>
+                        <div class="social-container">
+                            <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+                            <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <a href="#">Mot de passe oublié?</a>
+                        <button>Se connecter</button>
+                    </form>
+                </div>
+                <div class="overlay-container">
+                    <div class="overlay">
+                        <div class="overlay-panel overlay-left">
+                            <h1>Content de vous revoir !</h1>
+                            <p>
+                                Hâte de vous présenter nos nouveautés
+                            </p>
+                            <button class="ghost" id="signIn">Se connecter</button>
+                        </div>
+                        <div class="overlay-panel overlay-right">
+                            <h1>Hello, Lecteur !</h1>
+                            <p>Rejoins nos lecteurs pour leurs raconter ton histoire</p>
+                            <button class="ghost" id="signUp">s'enregistrer</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </section>
+        </section>
     </main>
-    <script src="./main.js"></script>
-  </body>
+    <script src="../connect.js"></script>
+</body>
+
 </html>
 </body>
+
 </html>
-
-
--- index.php
+++ index.php
@@ -1,28 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Biblook</title>
     <link rel="stylesheet" href="./css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Emilys+Candy&family=Hind+Madurai&family=Nova+Slim&display=swap" rel="stylesheet">
 </head>
+
 <body>
     <header id="main-header">
-    <nav id="main-nav-bar" class="row-limit-size">
-        <div id="container-group-logo">
-            <img src="./img/logo-biblook (3).svg" alt="Biblook">
-            <span id="nav-logo"></span>
-        </div>
-        <div id="container-group-search-nav-top">
-            <input type="text" id="input-search-nav-top">
-            <a href="#" id="btn-search-nav-top">Rechercher</a>
-        </div>
-        <div id="container-group-btn-connexion">
-            <a href="./front/connect.php" id="btn-log-in">Log in</a>
-            <a href="./front/connect.php" id="btn-sign-up" >Sign up</a>
-        </div>
-    </nav>
+        <nav id="main-nav-bar" class="row-limit-size">
+            <div id="container-group-logo">
+                <img src="./img/logo-biblook (3).svg" alt="Biblook">
+                <span id="nav-logo"></span>
+            </div>
+            <div id="container-group-search-nav-top">
+                <input type="text" id="input-search-nav-top">
+                <a href="#" id="btn-search-nav-top">Rechercher</a>
+            </div>
+            <div id="container-group-btn-connexion">
+                <a href="./front/connect.php" id="btn-log-in">Log in</a>
+                <a href="./front/connect.php" id="btn-sign-up">Sign up</a>
+            </div>
+        </nav>
     </header>
 </body>
+
 </html>
