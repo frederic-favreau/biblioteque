d1f2201fc0b84c0249d6535495c2b543e480759b
frederic-favreau <frederic.favreau22@gmail.com>
2023-03-02 20:02:36 +0100

    Update font all pages

--- index.php
+++ index.php
@@ -9,14 +9,14 @@
     <link rel="stylesheet" href="./css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Emilys+Candy&family=Hind+Madurai&family=Nova+Slim&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Hind+Madurai&family=Nova+Slim&family=Risque&display=swap" rel="stylesheet">
 </head>
 
 <body>
     <header id="main-header">
         <nav id="main-nav-bar" class="row-limit-size">
             <div id="container-group-logo">
-                <img src="./img/logo-seul-biblook-noir.svg"alt="Biblook">
+                <img src="./img/logo-seul-biblook-noir.svg" alt="Biblook">
                 <span id="nav-logo"></span>
             </div>
             <div id="container-group-search-nav-top">
@@ -26,12 +26,12 @@
             <div id="container-group-btn-connexion">
                 <a href="./front/connect.php" id="btn-log-in">Log in</a>
                 <a href="./front/connect.php" id="btn-sign-up"><span>Sign up</span>
-                <div id="menu-burger">
-                    <div class="menu-burger-pipe" id="menu-burger-pipe-top"></div>
-                    <div class="menu-burger-pipe" id="menu-burger-pipe-middle"></div>
-                    <div class="menu-burger-pipe" id="menu-burger-pipe-bottom"></div>
-                </div>
-            </a>
+                    <div id="menu-burger">
+                        <div class="menu-burger-pipe" id="menu-burger-pipe-top"></div>
+                        <div class="menu-burger-pipe" id="menu-burger-pipe-middle"></div>
+                        <div class="menu-burger-pipe" id="menu-burger-pipe-bottom"></div>
+                    </div>
+                </a>
             </div>
         </nav>
     </header>
