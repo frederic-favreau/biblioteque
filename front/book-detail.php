336d06d7d9d0c24baa5f4a777d5cb2054bd153d5
frederic-favreau <frederic.favreau22@gmail.com>
2023-03-03 18:29:53 +0100

    Ajout de book-detail + header-default

---
+++ front/book-detail.php
@@ -0,0 +1,24 @@
+<?php include_once '../front/header-default.php'; ?>
+    <h1>Livre detail</h1>
+
+    <h2>title work</h2>
+    <img src="pict" alt="Titre du livre ici">
+    <ul>
+        <li>extract</li>
+        <li>publish_at</li>
+        <li>new</li>
+        <li>date de publication</li>
+        <li>isbn</li>
+        <li>copy stock</li>
+        <li>location</li>
+        <li>editor name </li>
+        <li>editor date</li>
+        <li>category</li>
+        <li>genre</li>
+        <li>author</li>
+    </ul>
+    
+
+
+</body>
+</html>
---
+++ front/header-default.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biblook</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hind+Madurai&family=Nova+Slim&family=Risque&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    
+<!-- test -->
+
+    <!-- ---------- SECTION - HEADER - NAV - TOP ---------- -->
+
+
+    <header id="main-header">
+        <nav id="main-nav-bar" class="row-limit-size">
+            <div id="container-group-logo">
+                <img src="../img/logo-seul-biblook-noir.svg" alt="Biblook">
+                <span id="nav-logo-text">Biblook</span>
+            </div>
+            <div id="container-group-search-nav-top">
+                <input type="text" id="input-search-nav-top" placeholder="Taper votre recherche">
+                <a href="#" id="btn-search-nav-top">Rechercher</a>
+            </div>
+            <div id="container-group-btn-connexion">
+                <a href="./front/connect.php" id="btn-log-in">Log in</a>
+                <a href="./front/connect.php" id="btn-sign-up"><span>Sign up</span>
+                    <div id="menu-burger">
+                        <div class="menu-burger-pipe" id="menu-burger-pipe-top"></div>
+                        <div class="menu-burger-pipe" id="menu-burger-pipe-middle"></div>
+                        <div class="menu-burger-pipe" id="menu-burger-pipe-bottom"></div>
+                    </div>
+                </a>
+            </div>
+        </nav>
+    </header>
