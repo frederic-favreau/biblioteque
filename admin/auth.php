08e2fd78dfcfac53c2904ea83a240640d6513c78
sg.jiri <sg.jiri@yahoo.fr>
2023-03-03 08:49:26 +0100

    singup2

--- admin/auth.php
+++ admin/auth.php
@@ -6,11 +6,7 @@ require_once '../connexion.php';
 $mail= $_POST['mail'];
 $password = $_POST['password'];
 
-<<<<<<< HEAD
-$req = $db->prepare("SELECT `id_user`, `mail`, `lastname`, `firstname`, `password` FROM `user` WHERE `mail` = :mail");
-=======
 $req = $db->prepare("SELECT `id_user`, `mail`, `firstname`, 'lastname', `password` FROM `user` WHERE `mail` = :mail");
->>>>>>> aed8d9b79a64b4790ee4e83e316769815b5f7d13
 $req->bindParam('mail', $mail, PDO::PARAM_STR);
 $req->execute();
 
@@ -20,13 +16,7 @@ if ($req->rowCount()==1){
 
     if($user['password'] === $password){
         $_SESSION['id-user'] = $user['id_user'];
-<<<<<<< HEAD
         //$_SESSION['lastname'] = $user['lastname'];
-=======
-        $_SESSION['mail'] = $user['mail'];
-        $_SESSION['firstname'] = $user['firstname'];
-        $_SESSION['lastname'] = $user['lastname'];
->>>>>>> aed8d9b79a64b4790ee4e83e316769815b5f7d13
         header('Location: ./dashboard.php');
 
     } else {
