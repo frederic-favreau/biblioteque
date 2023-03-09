da099173cfc9b2cf0bc65686c4a5c8ad9da3ac61
sg.jiri <sg.jiri@yahoo.fr>
2023-03-09 16:59:22 +0100

    Biblook vous recommande

--- front/catalog.php
+++ front/catalog.php
@@ -10,7 +10,7 @@ include_once '../connexion.php';
 
 
     <section id="section-catalog" class="row-limit-size">
-        <h1 id="value-search">"Saisie de la recherche"</h1>
+        <h1 id="value-search">"<?= $_GET['search'] ?>"</h1>
         <div id="container-catalog">
             <div id="container-filter">
                 <p id="filter-title">Filtres</p>
@@ -107,25 +107,95 @@ include_once '../connexion.php';
                     <a href="#" id="help-choise">🔎 Me laisser guider </a>
                 </div>
                 <?php
-                $sql_catalog =
+
+               
+                if (isset($_GET['search']) and !empty($_GET['search'])) {
+                    $recheche = htmlspecialchars($_GET['search']);
+
+
+                    $sql =
+                    'SELECT DISTINCT `id_work`,`title`,`pict`,`extract`, 
+                    GROUP_CONCAT(DISTINCT `genre`.`name`) AS `genres`, 
+                    GROUP_CONCAT(DISTINCT CONCAT(`author`.`lastname`, SPACE(1), `author`.`firstname`)) AS `authors` 
+                    FROM `work`
+
+                    INNER JOIN `work_genre` 
+                    ON `work`.`id_work` = `work_genre`.`work_id`
+
+                    INNER JOIN `genre`
+                    ON `work_genre`.`genre_id` =`genre`.`id_genre`
+
+                    INNER JOIN `work_author`
+                    ON `work_author`.`work_id` = `work`.`id_work`
+
+                    INNER JOIN `author`
+                    ON `work_author`.`author_id` = `author`.`id_author`
+                    WHERE `title` LIKE "%' . $recheche . '%" OR  `author`.`lastname` LIKE "%' . $recheche . '%"  OR  `author`.`firstname` LIKE "%' . $recheche . '%" OR `genre`.`name` LIKE "%' . $recheche . '%"
+                    GROUP BY `id_work` ORDER BY `id_work` DESC';
+                    $req_catalog = $db->query($sql);
+
+                }
+                if ($req_catalog->rowCount()>0){
+                    while ($card= $req_catalog ->fetch(PDO::FETCH_ASSOC)) {
+                        ?>
+        
+        
+        
+                            <div class="card">
+                                <div class="top-item-card">
+                                    <img src="../img/books/<?= $card['pict'] ?>" alt="<?= $card['title'] ?>">
+                                </div>
+                                <div class="bottom-item-card">
+                                    <h4><?= str_replace(',', ', ', $card['genres']) ?></h4>
+                                    <h3><?= $card['title'] ?></h3>
+                                    <p class="description-card"><?= $card['extract'] ?></p>
+                                    <h5><?= str_replace(',', ', ', $card['authors']) ?></h5>
+                                    <a href="./book-detail.php?id=<?= $card['id_work'] ?>" class="link-page">En savoir plus ...</a>
+                                </div>
+                            </div>
+                            <?php }    
+                }else{
+                    ?>
+                        <p> Aucan livre trouvé</p>
+
+                    <?php
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                /*$sql =
                     "SELECT DISTINCT `id_work`,`title`,`pict`,`extract`, 
-                GROUP_CONCAT(DISTINCT `genre`.`name`) AS `genres`, 
-                GROUP_CONCAT(DISTINCT CONCAT(`author`.`lastname`, SPACE(1), `author`.`firstname`)) AS `authors` 
-                FROM `work`
+                    GROUP_CONCAT(DISTINCT `genre`.`name`) AS `genres`, 
+                    GROUP_CONCAT(DISTINCT CONCAT(`author`.`lastname`, SPACE(1), `author`.`firstname`)) AS `authors` 
+                    FROM `work`
 
-                INNER JOIN `work_genre` 
-                ON `work`.`id_work` = `work_genre`.`work_id`
+                    INNER JOIN `work_genre` 
+                    ON `work`.`id_work` = `work_genre`.`work_id`
 
-                INNER JOIN `genre`
-                ON `work_genre`.`genre_id` =`genre`.`id_genre`
+                    INNER JOIN `genre`
+                    ON `work_genre`.`genre_id` =`genre`.`id_genre`
 
-                INNER JOIN `work_author`
-                ON `work_author`.`work_id` = `work`.`id_work`
+                    INNER JOIN `work_author`
+                    ON `work_author`.`work_id` = `work`.`id_work`
 
-                INNER JOIN `author`
-                ON `work_author`.`author_id` = `author`.`id_author`
+                    INNER JOIN `author`
+                    ON `work_author`.`author_id` = `author`.`id_author`
 
-                GROUP BY `id_work` ORDER BY `id_work` DESC";
+                    GROUP BY `id_work` ORDER BY `id_work` DESC";
                 $req_catalog = $db->query($sql_catalog);
                 while ($card = $req_catalog->fetch(PDO::FETCH_ASSOC)) {
                 ?>
@@ -145,7 +215,7 @@ include_once '../connexion.php';
                         </div>
                     </div>
 
-                <?php } ?>
+                <?php } */ ?>
 
             </div>
         </div>
