0137d4e66a6d4603f9abae921271ce0da8d2a67c
elisa-1987 <elisaravinet@icloud.com>
2023-03-09 09:57:51 +0100

    html blog

--- blog.php
+++ blog.php
@@ -1,14 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>blog biblook</title>
     <link rel="stylesheet" href="./css/style-blog.css">
 </head>
+
 <body>
-    <h1>iuiuiui</h1>
-    <p>khkhki</p>
+
+    <main>
+        <section id="section-article" class="row-limit-size">
+            <h1>Journée Portes Ouvertes</h1>
+            <img src="./img/photosblog/ballons-removebg-preview 1.png" alt="bouquet de ballons">
+
+            <h2>Bibliothèque Biblook Saint-Denis-les-Bourg</h2>
+            <h3> Mercredi 21 Juin 2023 de 10h à 17h</h3>
+
+            <div id="containerpub">
+                <p>Entrée gratuite</p>
+                <p>Ouvert à tous</p>
+            </div>
+            <img src="./img/photosblog/cotillons-removebg-preview 1.png">
+            <div id="descriptif">
+                <div>Visite guidée</div>
+                <div>Vente de livres</div>
+                <div>Atelier lecture pour les enfants</div>
+            </div>
+            <hr>
+            <h4>Venez troquer des boutures!</h4>
+            <img src="./img/photosblog/floral-removebg-preview 1" alt="RDV le 24 avril 2023 à 14h">
+            <div>
+                <p>RDV le 24 avril 2023 à 14h</p>
+            </div>
+            <div id="activités">
+                <div>Partager des conseils de culture</div>
+                <div>Discuter jardinage</div>
+                <div>Se conseiller</div>
+                <div>Echanger des idées</div>
+
+            </div>
+
+        </section>
+    </main>
 </body>
+
 </html>
