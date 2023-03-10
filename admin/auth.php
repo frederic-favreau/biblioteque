c49f725cc735ab77ca846b16fa93d73a3135ada1
sg.jiri <sg.jiri@yahoo.fr>
2023-03-10 16:49:50 +0100

    modifier

--- admin/auth.php
+++ admin/auth.php
@@ -6,7 +6,7 @@ require_once '../connexion.php';
 $mail= $_POST['mail'];
 $password = $_POST['password'];
 
-$req = $db->prepare("SELECT `id_user`, `mail`, `firstname`, 'lastname', `password` FROM `user` WHERE `mail` = :mail");
+$req = $db->prepare("SELECT `id_user`, `mail`, `firstname`, `lastname`, `password` FROM `user` WHERE `mail` = :mail");
 $req->bindParam('mail', $mail, PDO::PARAM_STR);
 $req->execute();
 
@@ -16,8 +16,12 @@ if ($req->rowCount()==1){
 
     if($user['password'] === $password){
         $_SESSION['id-user'] = $user['id_user'];
-        //$_SESSION['lastname'] = $user['lastname'];
+        $_SESSION['firstname'] = $user['firstname'];
+        $_SESSION['lastname'] = $user['lastname'];
+        $_SESSION['mail'] = $user['mail'];
         header('Location: ./dashboard.php');
+        
+
 
     } else {
         header('Location: ../front/connect.php?err=1');
