973358e2d7b12c5099cf2fb3649aa9a9bdcb146a
Frédéric Favreau <frederic.favreau.dev@gmail.com>
2023-03-19 23:14:02 +0100

    tous les logout ok

--- admin/header-main.php
+++ admin/header-main.php
@@ -26,20 +26,35 @@ if (!isset($_SESSION['id-user'])) {
             <div id="sidebar-wrapper">
                 <nav id="main-sidebar">
                     <div id="container-top-navigation">
-                        <a href="../index.php" id="logo-sidebar"><img src="../img/logo-seul-biblook-noir.svg" alt="Bliblook" id="logo-big-sidebar"/><span id="dashbord-title"></span> Dashboard</a>
+                        <a href="../index.php" id="logo-sidebar"><img src="../img/logo-seul-biblook-noir.svg" alt="Bliblook" id="logo-big-sidebar" /><span id="dashbord-title">Tableau de bord</span></a>
                         <ul class="list-btn-side-bar">
-                            <li><a href="./dashboard.php"><img src="../img/side-bar/picto-home.svg" alt="accueil" id="btn-home">Mon accueil</a></li>
-                            <li><a href="./crud-book.php"><img src="../img/side-bar/picto-CRUD.svg" alt="crud">Gérer les livres</a></li>
-                            <li><a href="./register-book.php"><img src="../img/picto/bookmark.svg" alt="crud">Registre des emprunts</a></li>
-                            <li><a href="#"><img src="../img/side-bar/picto-users.svg" alt="utilisateur">Gérer les utilisateurs</a></li>
-                            <li><a href="../front/catalog.php"><img src="../img/picto/search-lg.svg" alt="utilisateur">Consulter le catalogue</a></li>
-                            
+                            <li>
+                                <a href="./dashboard.php"><img src="../img/side-bar/picto-home.svg" alt="accueil" id="btn-home">Mon accueil</a>
+                            </li>
+                            <li>
+                                <a href="./crud-book.php"><img src="../img/side-bar/picto-CRUD.svg" alt="crud">Gérer les livres</a>
+                            </li>
+                            <li>
+                                <a href="./register-book.php"><img src="../img/picto/bookmark.svg" alt="crud">Registre des emprunts</a>
+                            </li>
+                            <li>
+                                <a href="#"><img src="../img/side-bar/picto-users.svg" alt="utilisateur">Gérer les utilisateurs</a>
+                            </li>
+                            <li>
+                                <a href="../front/catalog.php"><img src="../img/picto/search-lg.svg" alt="utilisateur">Consulter le catalogue</a>
+                            </li>
+
                         </ul>
                     </div>
                     <div id="container-group-profil">
                         <ul>
-                            <li><a href="#" id="settings"><img src="../img/side-bar/picto-settings.svg" alt="Options">Options</a></li>
-                            <li><a href="./profil.php" id="profil"><img src="../img/side-bar/Avatar.svg" alt="prenom" id="profil-pict">Mon profil</a></li>
+                            <li>
+                                <a href="#" id="settings"><img src="../img/side-bar/picto-settings.svg" alt="Options">Options</a>
+                            </li>
+                            <li>
+                                <a href="./profil.php" id="profil"><img src="../img/side-bar/Avatar.svg" alt="prenom" id="profil-pict">Mon profil</a>
+                                <a href="./logout.php"><img src="../img/side-bar/leave-session.svg" alt="Quitter"></a>
+                            </li>
                         </ul>
                     </div>
                 </nav>
