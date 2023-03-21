dd575c21da4df245f38875882d2e61483580cb8b
sg.jiri <sg.jiri@yahoo.fr>
2023-03-21 21:01:46 +0100

    delete like

--- admin/delete-like.php
+++ admin/delete-like.php
@@ -8,10 +8,18 @@ require_once '../connexion.php';
 $idWork = $_GET['id'];
 $idUser = $_SESSION['id-user'];
 
-if (isset($_POST['delete'])) {
-    $delete = $db->prepare('DELETE FROM `like` WHERE `work_id` = :work_id AND user_id = :user_id');
-    $delete->bindParam('work_id', $workId, PDO::PARAM_INT);
-    $delete->bindParam(':user_id', $idUser, PDO::PARAM_INT);
-    $delete->execute();
-    //header('Location: ../admin/dashboard.php');
-}
+
+
+                    
+
+                    $likeSql = $db->prepare("DELETE FROM `like` WHERE `work_id` = :work_id AND user_id = :user_id");
+
+                    $likeSql->bindParam(':work_id', $idWork, PDO::PARAM_INT);
+                    $likeSql->bindParam(':user_id', $idUser, PDO::PARAM_INT);
+
+                    $likeSql->execute();
+                    header('Location: ../admin/dashboard.php');
+
+
+
+
