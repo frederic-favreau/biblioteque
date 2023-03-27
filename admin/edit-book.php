f48d0b5dd77a0043667a434b58a61140bcae38ea
frederic-favreau <frederic.favreau22@gmail.com>
2023-03-27 09:31:41 +0200

    push

--- admin/edit-book.php
+++ admin/edit-book.php
@@ -22,14 +22,14 @@ include_once '../admin/header-main.php';
                 $authorLastname = ($_POST['author-lastname-edit']);
                 $authorFirstname2 = ($_POST['author-firstname-2-edit']);
                 $authorLastname2 = ($_POST['author-2-lastname-edit']);
-                
+
                 $genre = ($_POST['work-genre-edit']);
                 $genre2 = ($_POST['work-genre-2-edit']);
                 $publishedDate = ($_POST['work-publish-date-edit']);
                 $LCCN = ($_POST['work-LCCN-edit']);
                 $extract = ($_POST['work-extract-edit']);
                 $workPict = ($_POST['work-pict-edit']);
-               
+
                 // Requête pour mettre à jour le livre
                 $reqBookNewedit = $db->prepare("UPDATE `work` SET `title` = :title, `published_at` = :publishedDate, `LCCN` = :LCCN, `extract` = :extract, `pict` = :workPict WHERE `id_work`= :id");
                 $reqBookNewedit->bindParam(':title', $title);
@@ -67,11 +67,6 @@ include_once '../admin/header-main.php';
                     $reqUpdateWorkAuthor->bindParam(':id', $id, PDO::PARAM_INT);
                     $reqUpdateWorkAuthor->execute();
                 }
-
-
-
-
-
 
                 if (isset($authorFirstname2, $authorLastname2)) {
 
@@ -144,130 +139,93 @@ include_once '../admin/header-main.php';
                     }
                 }
 
+                // Vérifier si le genre existe déjà
+                $reqGenreExists = $db->prepare("SELECT `id_genre` FROM `genre` WHERE `name` = :genre");
+                $reqGenreExists->bindParam(':genre', $genre);
+                $reqGenreExists->execute();
+                $genreExists = $reqGenreExists->fetch(PDO::FETCH_ASSOC);
+
+                if ($genreExists) {
+
+                    // Le genre existe déjà, il suffit de mettre à jour la table work_genre
+                    $genreId = $genreExists['id_genre'];
+                    $reqUpdateWorkGenre = $db->prepare("UPDATE `work_genre`  SET `genre_id` = :genreId WHERE `work_id` = :id");
+                    $reqUpdateWorkGenre->bindParam(':genreId', $genreId);
+                    $reqUpdateWorkGenre->bindParam(':id', $id, PDO::PARAM_INT);
+                    $reqUpdateWorkGenre->execute();
+                } else {
+                    // Le genre n'existe pas, insérer le nouveau genre et mettre à jour la table work_genre
+                    $reqInsertGenre = $db->prepare("INSERT INTO `genre` (`name`) VALUES (:genre)");
+                    $reqInsertGenre->bindParam(':genre', $genre);
+                    $reqInsertGenre->execute();
+                    $newGenreId = $db->lastInsertId();
+
+                    $reqUpdateWorkGenre = $db->prepare("UPDATE `work_genre` SET `genre_id` = :newGenreId WHERE `work_id` = :id");
+                    $reqUpdateWorkGenre->bindParam(':newGenreId', $newGenreId);
+                    $reqUpdateWorkGenre->bindParam(':id', $id, PDO::PARAM_INT);
+                    $reqUpdateWorkGenre->execute();
+                }
 
 
+                if (isset($genre2)) {
+                    // Vérifier si le genre2 existe déjà
+                    $reqGenre2Exists = $db->prepare("SELECT `id_genre` FROM `genre` WHERE `name` = :genre2");
+                    $reqGenre2Exists->bindParam(':genre2', $genre2);
+                    $reqGenre2Exists->execute();
+                    $genre2Exists = $reqGenre2Exists->fetch(PDO::FETCH_ASSOC);
 
+                    if ($genre2Exists) {
+                        // Le genre existe déjà, vérifier si la relation work_genre existe pour l'œuvre et le genre
+                        $genre2Id = $genre2Exists['id_genre'];
+                        $reqWorkGenre2Exists = $db->prepare("SELECT `id_work_genre` FROM `work_genre` WHERE `work_id` = :work_id AND `genre_id` = :genre_id");
+                        $reqWorkGenre2Exists->bindParam(':work_id', $id, PDO::PARAM_INT);
+                        $reqWorkGenre2Exists->bindParam(':genre_id', $genre2Id, PDO::PARAM_INT);
+                        $reqWorkGenre2Exists->execute();
+                        $workGenre2Exists = $reqWorkGenre2Exists->fetch(PDO::FETCH_ASSOC);
 
+                        if ($workGenre2Exists) {
+                            // La relation work_genre existe déjà, mettre à jour la ligne correspondante avec l'id_genre récupéré
+                            $reqUpdateWorkGenre2 = $db->prepare("UPDATE `work_genre` SET `genre_id` = :genre_id WHERE `id_work_genre` = :id_work_genre");
+                            $reqUpdateWorkGenre2->bindParam(':genre_id', $genre2Id, PDO::PARAM_INT);
+                            $reqUpdateWorkGenre2->bindParam(':id_work_genre', $workGenre2Exists['id_work_genre'], PDO::PARAM_INT);
+                            $reqUpdateWorkGenre2->execute();
+                        } else {
+                            // La relation work_genre n'existe pas, insérer une nouvelle ligne dans la table work_genre avec l'œuvre et l'id_genre récupéré
+                            $newRelationSql = $db->prepare("INSERT INTO `work_genre`(`work_id`,`genre_id`) VALUES (:work_id, :genre_id)");
+                            $newRelationSql->bindParam(':work_id', $id, PDO::PARAM_INT);
+                            $newRelationSql->bindParam(':genre_id', $genre2Id, PDO::PARAM_INT);
+                            $newRelationSql->execute();
+                        }
+                    } else {
+                        // Le genre n'existe pas, insérer le nouveau genre et mettre à jour la table work_genre
+                        $reqInsertGenre2 = $db->prepare("INSERT INTO `genre` (`name`) VALUES (:genre2)");
+                        $reqInsertGenre2->bindParam(':genre2', $genre2);
+                        $reqInsertGenre2->execute();
+                        $newGenre2Id = $db->lastInsertId();
 
+                        // Vérifier si la deuxième relation existe déjà
+                        $reqGenre_Work2Exists = $db->prepare("SELECT * FROM `work_genre` WHERE `work_id` = :id AND `genre_id` = :newGenreId ORDER BY `id_work_genre` DESC LIMIT 1");
+                        $reqGenre_Work2Exists->bindParam(':id', $id, PDO::PARAM_INT);
+                        $reqGenre_Work2Exists->bindParam(':newGenreId', $newGenre2Id, PDO::PARAM_INT);
+                        $reqGenre_Work2Exists->execute();
+                        $Genre_Work2Exists = $reqGenre_Work2Exists->fetch(PDO::FETCH_ASSOC);
 
-
-
-
-
-
-
-
-// Vérifier si le genre existe déjà
-$reqGenreExists = $db->prepare("SELECT `id_genre` FROM `genre` WHERE `name` = :genre");
-$reqGenreExists->bindParam(':genre', $genre);
-$reqGenreExists->execute();
-$genreExists = $reqGenreExists->fetch(PDO::FETCH_ASSOC);
-
-if ($genreExists) {
-
-    // Le genre existe déjà, il suffit de mettre à jour la table work_genre
-    $genreId = $genreExists['id_genre'];
-    $reqUpdateWorkGenre = $db->prepare("UPDATE `work_genre`  SET `genre_id` = :genreId WHERE `work_id` = :id");
-    $reqUpdateWorkGenre->bindParam(':genreId', $genreId);
-    $reqUpdateWorkGenre->bindParam(':id', $id, PDO::PARAM_INT);
-    $reqUpdateWorkGenre->execute();
-} else {
-    // Le genre n'existe pas, insérer le nouveau genre et mettre à jour la table work_genre
-    $reqInsertGenre = $db->prepare("INSERT INTO `genre` (`name`) VALUES (:genre)");
-    $reqInsertGenre->bindParam(':genre', $genre);
-    $reqInsertGenre->execute();
-    $newGenreId = $db->lastInsertId();
-
-    $reqUpdateWorkGenre = $db->prepare("UPDATE `work_genre` SET `genre_id` = :newGenreId WHERE `work_id` = :id");
-    $reqUpdateWorkGenre->bindParam(':newGenreId', $newGenreId);
-    $reqUpdateWorkGenre->bindParam(':id', $id, PDO::PARAM_INT);
-    $reqUpdateWorkGenre->execute();
-}
-
-
-
-
-
-
-
-
-
-if (isset($genre2)) {
-    // Vérifier si le genre2 existe déjà
-    $reqGenre2Exists = $db->prepare("SELECT `id_genre` FROM `genre` WHERE `name` = :genre2");
-    $reqGenre2Exists->bindParam(':genre2', $genre2);
-    $reqGenre2Exists->execute();
-    $genre2Exists = $reqGenre2Exists->fetch(PDO::FETCH_ASSOC);
-
-    if ($genre2Exists) {
-        // Le genre existe déjà, vérifier si la relation work_genre existe pour l'œuvre et le genre
-        $genre2Id = $genre2Exists['id_genre'];
-        $reqWorkGenre2Exists = $db->prepare("SELECT `id_work_genre` FROM `work_genre` WHERE `work_id` = :work_id AND `genre_id` = :genre_id");
-        $reqWorkGenre2Exists->bindParam(':work_id', $id, PDO::PARAM_INT);
-        $reqWorkGenre2Exists->bindParam(':genre_id', $genre2Id, PDO::PARAM_INT);
-        $reqWorkGenre2Exists->execute();
-        $workGenre2Exists = $reqWorkGenre2Exists->fetch(PDO::FETCH_ASSOC);
-
-        if ($workGenre2Exists) {
-            // La relation work_genre existe déjà, mettre à jour la ligne correspondante avec l'id_genre récupéré
-            $reqUpdateWorkGenre2 = $db->prepare("UPDATE `work_genre` SET `genre_id` = :genre_id WHERE `id_work_genre` = :id_work_genre");
-            $reqUpdateWorkGenre2->bindParam(':genre_id', $genre2Id, PDO::PARAM_INT);
-            $reqUpdateWorkGenre2->bindParam(':id_work_genre', $workGenre2Exists['id_work_genre'], PDO::PARAM_INT);
-            $reqUpdateWorkGenre2->execute();
-        } else {
-            // La relation work_genre n'existe pas, insérer une nouvelle ligne dans la table work_genre avec l'œuvre et l'id_genre récupéré
-            $newRelationSql = $db->prepare("INSERT INTO `work_genre`(`work_id`,`genre_id`) VALUES (:work_id, :genre_id)");
-            $newRelationSql->bindParam(':work_id', $id, PDO::PARAM_INT);
-            $newRelationSql->bindParam(':genre_id', $genre2Id, PDO::PARAM_INT);
-            $newRelationSql->execute();
-        }
-    } else {
-        // Le genre n'existe pas, insérer le nouveau genre et mettre à jour la table work_genre
-        $reqInsertGenre2 = $db->prepare("INSERT INTO `genre` (`name`) VALUES (:genre2)");
-        $reqInsertGenre2->bindParam(':genre2', $genre2);
-        $reqInsertGenre2->execute();
-        $newGenre2Id = $db->lastInsertId();
-
-   // Vérifier si la deuxième relation existe déjà
-$reqGenre_Work2Exists = $db->prepare("SELECT * FROM `work_genre` WHERE `work_id` = :id AND `genre_id` = :newGenreId ORDER BY `id_work_genre` DESC LIMIT 1");
-$reqGenre_Work2Exists->bindParam(':id', $id, PDO::PARAM_INT);
-$reqGenre_Work2Exists->bindParam(':newGenreId', $newGenre2Id, PDO::PARAM_INT);
-$reqGenre_Work2Exists->execute();
-$Genre_Work2Exists = $reqGenre_Work2Exists->fetch(PDO::FETCH_ASSOC);
-
-if ($Genre_Work2Exists) {
-    // La deuxième relation existe déjà, mettre à jour la table work_genre
-    $genre2Id = $newGenre2Id;
-    $reqUpdateWorkGenre2 = $db->prepare("UPDATE `work_genre` SET `genre_id` = :genre2Id WHERE `id_work_genre` = :id_work_genre");
-    $reqUpdateWorkGenre2->bindParam(':genre2Id', $genre2Id, PDO::PARAM_INT);
-    $reqUpdateWorkGenre2->bindParam(':id_work_genre', $Genre_Work2Exists['id_work_genre'], PDO::PARAM_INT);
-    $reqUpdateWorkGenre2->execute();
-} else {
-    // La deuxième relation n'existe pas, insérer une nouvelle ligne dans la table work_genre avec l'œuvre et l'id_genre récupéré
-    $newRelationSql = $db->prepare("INSERT INTO `work_genre`(`work_id`,`genre_id`) VALUES (:work_id, :genre2Id)");
-    $newRelationSql->bindParam(':work_id', $id, PDO::PARAM_INT);
-    $newRelationSql->bindParam(':genre2Id', $newGenre2Id, PDO::PARAM_INT);
-    $newRelationSql->execute();
-}}}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                        if ($Genre_Work2Exists) {
+                            // La deuxième relation existe déjà, mettre à jour la table work_genre
+                            $genre2Id = $newGenre2Id;
+                            $reqUpdateWorkGenre2 = $db->prepare("UPDATE `work_genre` SET `genre_id` = :genre2Id WHERE `id_work_genre` = :id_work_genre");
+                            $reqUpdateWorkGenre2->bindParam(':genre2Id', $genre2Id, PDO::PARAM_INT);
+                            $reqUpdateWorkGenre2->bindParam(':id_work_genre', $Genre_Work2Exists['id_work_genre'], PDO::PARAM_INT);
+                            $reqUpdateWorkGenre2->execute();
+                        } else {
+                            // La deuxième relation n'existe pas, insérer une nouvelle ligne dans la table work_genre avec l'œuvre et l'id_genre récupéré
+                            $newRelationSql = $db->prepare("INSERT INTO `work_genre`(`work_id`,`genre_id`) VALUES (:work_id, :genre2Id)");
+                            $newRelationSql->bindParam(':work_id', $id, PDO::PARAM_INT);
+                            $newRelationSql->bindParam(':genre2Id', $newGenre2Id, PDO::PARAM_INT);
+                            $newRelationSql->execute();
+                        }
+                    }
+                }
 
 
 
@@ -313,6 +271,22 @@ if ($Genre_Work2Exists) {
 
             $book = $req_book->fetch(PDO::FETCH_ASSOC);
 
+            $authorsLastName = explode(',', $book['lastname']);
+
+
+
+            $authorsFirstName = explode(',', $book['firstname']);
+            $genres = explode(',', $book['genre']);
+           
+
+            $authorsLastName = explode(',',$book['lastname']);
+
+            $authorsFirstName = explode(',',$book['firstname']);
+            $genres = explode(',',$book['genre']);
+
+
+
+
             ?>
 
 
@@ -332,7 +306,7 @@ if ($Genre_Work2Exists) {
                     </div>
                     <div class="add-form-template-label-input">
                         <label for="author-firstname-2-edit" class="label-form-add-book">Prénom de l'auteur (optionnel)</label>
-                        <input type="text" name="author-firstname-2-edit" id="author-firstname-2-edit" <?php if(isset($authorsFirstName[1])) {?> value="<?= $authorsFirstName[1] ?>" <?php } ?>class="input-form-add-book" />
+                        <input type="text" name="author-firstname-2-edit" id="author-firstname-2-edit" <?php if (isset($authorsFirstName[1])) { ?> value="<?= $authorsFirstName[1] ?>" <?php } ?>class="input-form-add-book" />
                     </div>
 
                     <div class="add-form-template-label-input">
